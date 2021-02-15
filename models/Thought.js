f6bb06f550f7d7040da08ce0b21dd3259b3a91b3
Racheal Colbert <rachealcolbert16@gmail.com>
2021-02-14 19:26:06 -0700

    reaction scheme created within thought model

--- models/Thought.js
+++ models/Thought.js
@@ -17,10 +17,31 @@ const SocialThought = new Schema({
     },
     username: {
         type: String,
-        Required: true
+        required: true
     },
-    reactions: [ReactionSchema]
+    reactions: [SocialReactions]
 });
 
+const SocialReactions = new Schema({
+    reactionID: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+
+})
 const Thought = model('Thought', SocialThought);
 module.exports = Thought;
