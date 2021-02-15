ce5685439e3fec40d0f647a85971f5d3eb1a0479
Racheal Colbert <rachealcolbert16@gmail.com>
2021-02-14 19:03:06 -0700

    Thought module set up along with date formatting

--- models/Thought.js
+++ models/Thought.js
@@ -2,14 +2,25 @@ const {
     Schema,
     model
 } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const SocialThought = new Schema({
     thoughtText: {
         type: String,
         required: true,
-    }
-})
-thoughtText 
-createdAt 
-username 
-reactions 
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        Required: true
+    },
+    reactions: [ReactionSchema]
+});
+
+const Thought = model('Thought', SocialThought);
+module.exports = Thought;
